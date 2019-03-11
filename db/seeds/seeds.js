d4f435628b6c1795997ed41b25b83bb80050f75d
george <longlandg@hotmail.com>
2019-03-11 17:24:30 +0000

    almost finished seedin no id

--- db/seeds/seeds.js
+++ db/seeds/seeds.js
@@ -1,7 +1,7 @@
 const {
   articleData, topicData, userData, commentData,
 } = require('../data');
-const { createTimeStamp } = require('../../utils/utils.js');
+const { createTimeStamp, createRef, createArticleIdLink } = require('../../utils/utils.js');
 
 exports.seed = function (knex, Promise) {
   return knex.migrate.rollback()
@@ -11,5 +11,32 @@ exports.seed = function (knex, Promise) {
     .then(() => {
       const formattedArticleData = createTimeStamp(articleData);
       return knex.insert(formattedArticleData).into('articles').returning('*');
+    })
+    .then((article) => {
+      // const title = article.title;
+      // const article_id = 'article_id';
+      const refObject = createRef(articleData, title, article_id);
+      return refObject;
+    })
+    .then((refObject) => {
+      const formattedComments = createArticleIdLink(commentData, refObject);
+      return knex.insert(formattedComments).into('comments').returning('*');
     });
 };
+
+
+// exports.createRef = (articleRows, title, article_id) => articleRows.reduce((acc, element) => {
+//   acc[element[title]] = element[article_id];
+//   return acc;
+// }, {});
+
+// exports.createArticleIdLink = (commentRows, refObject) => {
+//   const formattedComments = commentRows.map(comment => ({
+//     author: comment.created_by,
+//     article_id: refObject[comment.belongs_to],
+//     votes: comment.votes || 0,
+//     created_at: new Date(comment.created_at),
+//     body: comment.body,
+//   }));
+//   return formattedComments;
+// };
