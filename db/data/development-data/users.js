34aa9526c8224a2f7fcc94881b40eed5e513596f
george <longlandg@hotmail.com>
2019-06-02 10:41:01 +0100

    user profile image liks updated

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -1,32 +1,38 @@
 module.exports = [
   {
-    username: 'tickle122',
-    name: 'Tom Tickle',
-    avatar_url: 'https://www.spiritsurfers.net/monastery/wp-content/uploads/_41500270_mrtickle.jpg',
+    username: "tickle122",
+    name: "Tom Tickle",
+    avatar_url:
+      "https://www.spiritsurfers.net/monastery/wp-content/uploads/_41500270_mrtickle.jpg"
   },
   {
-    username: 'grumpy19',
-    name: 'Paul Grump',
-    avatar_url: 'https://www.tumbit.com/profile-image/4/original/mr-grumpy.jpg',
+    username: "grumpy19",
+    name: "Paul Grump",
+    avatar_url:
+      "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwi8s_aswMriAhV85-AKHacEDKsQjRx6BAgBEAU&url=https%3A%2F%2Fmrmen.fandom.com%2Fwiki%2FMr._Grumpy&psig=AOvVaw21Ku8NeXVbHkE9yGgBrogj&ust=1559554754768802"
   },
   {
-    username: 'happyamy2016',
-    name: 'Amy Happy',
-    avatar_url: 'https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729',
+    username: "happyamy2016",
+    name: "Amy Happy",
+    avatar_url:
+      "https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729"
   },
   {
-    username: 'cooljmessy',
-    name: 'Peter Messy',
-    avatar_url: 'https://i.imgur.com/WfX0Neu.jpg',
+    username: "cooljmessy",
+    name: "Peter Messy",
+    avatar_url:
+      "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwi_jOiTwMriAhVO0uAKHaU6AckQjRx6BAgBEAU&url=%2Furl%3Fsa%3Di%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fmrmen.fandom.com%252Fwiki%252FMr._Messy%26psig%3DAOvVaw2WIBogok0Arws2zHlnYvqk%26ust%3D1559554679917811&psig=AOvVaw2WIBogok0Arws2zHlnYvqk&ust=1559554679917811"
   },
   {
-    username: 'weegembump',
-    name: 'Gemma Bump',
-    avatar_url: 'https://www.upandrunning.co.uk/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/m/r/mr-bump.jpg',
+    username: "weegembump",
+    name: "Gemma Bump",
+    avatar_url:
+      "https://www.upandrunning.co.uk/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/m/r/mr-bump.jpg"
   },
   {
-    username: 'jessjelly',
-    name: 'Jess Jelly',
-    avatar_url: 'https://s-media-cache-ak0.pinimg.com/564x/39/62/ec/3962eca164e60cf46f979c1f57d4078b.jpg',
-  },
+    username: "jessjelly",
+    name: "Jess Jelly",
+    avatar_url:
+      "https://s-media-cache-ak0.pinimg.com/564x/39/62/ec/3962eca164e60cf46f979c1f57d4078b.jpg"
+  }
 ];
